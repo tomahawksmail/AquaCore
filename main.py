e291521c3969a3af892e20dede837468e8561aa0
tomahawksmail <tomahawksmail@gmail.com>
2023-11-21 16:48:47 -0800

    working

--- main.py
+++ main.py
@@ -77,96 +77,102 @@ def login():
 
 
 
-@app.route("/aquarium", methods=['POST', 'GET'])
-def dashboard():
-    # insert()
-    if 'user' in session:
-        if request.method == 'GET':
-            connection.connect()
-            SQLrequest = """SELECT DATE_FORMAT(DATETIME, '%D %H:%i'), temp, ph, tds FROM `data` ORDER BY id ASC LIMIT 48"""
-            min_temp_request = """SELECT min_temp FROM options"""
-            max_temp_request = """SELECT max_temp FROM options"""
-            min_ph_request = """SELECT min_ph FROM options"""
-            max_ph_request = """SELECT max_ph FROM options"""
-            tds_request = """SELECT tds FROM options"""
-            current = """SELECT temp, ph, tds FROM `data` ORDER BY id DESC LIMIT 1"""
+# @app.route("/aquarium", methods=['POST', 'GET'])
+# def dashboard():
+#     # insert()
+#     if 'user' in session:
+#         if request.method == 'GET':
+#             connection.connect()
+#             SQLrequest = """SELECT DATE_FORMAT(DATETIME, '%D %H:%i'), temp, ph, tds FROM `data` ORDER BY id ASC LIMIT 48"""
+#             min_temp_request = """SELECT min_temp FROM options"""
+#             max_temp_request = """SELECT max_temp FROM options"""
+#             min_ph_request = """SELECT min_ph FROM options"""
+#             max_ph_request = """SELECT max_ph FROM options"""
+#             tds_request = """SELECT tds FROM options"""
+#             current = """SELECT temp, ph, tds FROM `data` ORDER BY id DESC LIMIT 1"""
+#
+#             try:
+#                 with connection.cursor() as cursor:
+#                     cursor.execute(SQLrequest)
+#                 result = cursor.fetchall()
+#
+#                 with connection.cursor() as cursor:
+#                     cursor.execute(min_temp_request)
+#                 min = cursor.fetchone()[0]
+#                 min_temp = [min for i in range(48)]
+#
+#                 with connection.cursor() as cursor:
+#                     cursor.execute(max_temp_request)
+#                 max = cursor.fetchone()[0]
+#                 max_temp = [max for i in range(48)]
+#
+#                 with connection.cursor() as cursor:
+#                     cursor.execute(min_ph_request)
+#                 minph = cursor.fetchone()[0]
+#                 min_ph = [minph for i in range(48)]
+#
+#                 with connection.cursor() as cursor:
+#                     cursor.execute(max_ph_request)
+#                 maxph = cursor.fetchone()[0]
+#                 max_ph = [maxph for i in range(48)]
+#
+#                 with connection.cursor() as cursor:
+#                     cursor.execute(tds_request)
+#                 tds = cursor.fetchone()[0]
+#                 tds = [tds for i in range(48)]
+#
+#                 with connection.cursor() as cursor:
+#                     cursor.execute(current)
+#                 current = cursor.fetchone()
+#             except Exception as E:
+#                 return render_template('aquarium.html', version=version)
+#             else:
+#                 cursor.close()
+#                 connection.close()
+#             return render_template('aquarium.html', version=version, result=result, min_temp=min_temp, max_temp=max_temp, min_ph=min_ph, max_ph=max_ph, tds=tds, current=current)
+#         else:
+#             return render_template('aquarium.html', version=version, result=None)
+#     else:
+#         flash("You are not logged in")
+#         return redirect("/login")
 
-            try:
-                with connection.cursor() as cursor:
-                    cursor.execute(SQLrequest)
-                result = cursor.fetchall()
-
-                with connection.cursor() as cursor:
-                    cursor.execute(min_temp_request)
-                min = cursor.fetchone()[0]
-                min_temp = [min for i in range(48)]
-
-                with connection.cursor() as cursor:
-                    cursor.execute(max_temp_request)
-                max = cursor.fetchone()[0]
-                max_temp = [max for i in range(48)]
-
-                with connection.cursor() as cursor:
-                    cursor.execute(min_ph_request)
-                minph = cursor.fetchone()[0]
-                min_ph = [minph for i in range(48)]
-
-                with connection.cursor() as cursor:
-                    cursor.execute(max_ph_request)
-                maxph = cursor.fetchone()[0]
-                max_ph = [maxph for i in range(48)]
-
-                with connection.cursor() as cursor:
-                    cursor.execute(tds_request)
-                tds = cursor.fetchone()[0]
-                tds = [tds for i in range(48)]
-
-                with connection.cursor() as cursor:
-                    cursor.execute(current)
-                current = cursor.fetchone()
-
-
-
-
-
-            except Exception as E:
-                return render_template('aquarium.html', version=version)
-            else:
-                cursor.close()
-                connection.close()
-
-
-            return render_template('aquarium.html', version=version, result=result, min_temp=min_temp, max_temp=max_temp, min_ph=min_ph, max_ph=max_ph, tds=tds, current=current)
-        else:
-            return render_template('aquarium.html', version=version, result=None)
+def getDatatoOptions():
+    SQLrequest = """SELECT * FROM options"""
+    status = """SELECT * FROM status"""
+    try:
+        connection.connect()
+        with connection.cursor() as cursor:
+            cursor.execute(SQLrequest)
+        options = cursor.fetchone()
+        with connection.cursor() as cursor:
+            cursor.execute(status)
+        status = cursor.fetchall()[0]
+        result = (options, status)
+    except Exception as E:
+        print(E)
     else:
-        flash("You are not logged in")
-        return redirect("/login")
-
-
+        cursor.close()
+        connection.close()
+    return result
 
 @app.route("/options", methods=['POST', 'GET'])
 def options():
     if 'user' in session:
         if request.method == 'GET':
-            connection.connect()
-            SQLrequest = """SELECT * FROM options"""
-            status = """SELECT * FROM status"""
-            try:
-                with connection.cursor() as cursor:
-                    cursor.execute(SQLrequest)
-                result = cursor.fetchone()
-                with connection.cursor() as cursor:
-                    cursor.execute(status)
-                status = cursor.fetchall()[0]
-
-                print(status)
-            except Exception as E:
-                print(E)
+            result = getDatatoOptions()
+            return render_template('options.html', version=version, result=result[0], status=result[1])
         elif request.method == 'POST':
-            print("dvdsv")
-    return render_template('options.html', version=version, result=result, status=status)
-
+            if "submit" in request.form:
+                co2 = request.form.get("co2")
+                o2 = request.form.get("o2")
+                heater_status = request.form.get("heater_status")
+                Master_light_status = request.form.get("Master_light_status")
+                print(co2, o2, heater_status, Master_light_status)
+                result = getDatatoOptions()
+                return render_template('options.html', version=version, result=result[0], status=result[1])
+    else:
+        flash("You are not logged in")
+        return redirect("/login")
 @app.route("/alerts", methods=['POST', 'GET'])
 def alerts():
     return render_template('alerts.html', version=version)
@@ -174,27 +180,6 @@ def alerts():
 @app.route("/core_dashboard", methods=['POST', 'GET'])
 def core_dashboard():
     return render_template('core_dashboard.html', version=version)
-
-def insert():
-    import random
-    connection.connect()
-    for i in range(24):
-        temp = random.randint(25, 35)
-        ph   = round(random.uniform(4, 8))
-        tds  = random.randint(200, 800)
-        dttemp = "'" + str(datetime.strptime(f"2023-10-27 {i}:00:00", '%Y-%m-%d %H:%M:%S')) + "'"
-
-        sql = f"""INSERT INTO data (datetime, temp, ph, tds) VALUES ({dttemp}, {temp}, {ph}, {tds})"""
-        # print(sql)
-
-        try:
-            with connection.cursor() as cursor:
-                cursor.execute(sql)
-
-        except Exception as E:
-            print(E)
-        connection.commit()
-
 
 
 
