927b65b50df0e6b3785987a5f5f4a82bca2bc451
tomahawksmail <tomahawksmail@gmail.com>
2023-12-08 13:48:24 -0800

    work on WiFi

--- accesspoint.py
+++ accesspoint.py
@@ -1,7 +1,6 @@
 import subprocess
 
-host = 'http://192.168.150.5'
-port = 81
+host = '192.168.150.5'
 def startAP(): #hotspot
     subprocess.run(f'sudo create_ap -g {host} -n wlan0 AquaCore orangepi --no-virt'.split(),check=True)
 def checkwifi():
