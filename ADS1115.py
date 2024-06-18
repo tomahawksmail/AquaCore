80be766562449789dfdd87bcacb02deb1ec608cb
tomahawksmail <tomahawksmail@gmail.com>
2024-06-18 15:18:36 -0700

    time triggers

--- ADS1115.py
+++ ADS1115.py
@@ -7,12 +7,19 @@ delay = 1
 def analogportsread():
     value = 0
     try:
+        volts = adc.read_adc(3, gain=GAIN, data_rate=128)
+        temp = 21
+        averageVoltage = median(volts)
+        compensationCoefficient = 1.0 + 0.02 * (temp - 25.0)
+
+        compensationVolatge = averageVoltage / compensationCoefficient
+        tds = compensationVolatge * 0.000125
         val = adc.read_adc(3, gain=GAIN, data_rate=128)*5*1.686/65535
     except Exception as E:
         print(E)
-        val = 0
+        tds = 0
     else:
-        print(val)
+        print(tds)
 
 
 if __name__ == '__main__':
