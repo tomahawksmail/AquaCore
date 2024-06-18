4241a5c019b166918612059a451a554ba5e500d4
Maksym Tsybulskyi <66479202+tomahawksmail@users.noreply.github.com>
2024-06-18 08:26:00 +0300

    Update ADS1115.py

--- ADS1115.py
+++ ADS1115.py
@@ -2,12 +2,12 @@ import time
 import Adafruit_ADS1x15
 adc = Adafruit_ADS1x15.ADS1115(address=0x48, busnum=1)
 
-GAIN = 2
+GAIN = 1
 delay = 1
 def analogportsread():
     value = 0
     try:
-        val = adc.read_adc(3, gain=GAIN)
+        val = adc.read_adc(3, gain=GAIN, data_rate=128)
     except Exception as E:
         print(E)
         val = 0
